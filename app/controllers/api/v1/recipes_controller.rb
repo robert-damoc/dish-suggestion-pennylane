0d203c0de647d3f516a37eb733a43196ca0fa092
Robert <robert.damoc@yahoo.com>
2021-06-08 00:26:09 +0300

    Add recipes index and show

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RecipesController < ApplicationController
+  def index
+    recipe = Recipe.order('RANDOM()').first(5)
+    render json: recipe
+  end
+
+  def show
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
+  end
+
+  private
+
+  def recipe
+    @recipe ||= Recipe.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: %i[index show]
+    end
+  end
+
   root 'homepage#index'
 end
