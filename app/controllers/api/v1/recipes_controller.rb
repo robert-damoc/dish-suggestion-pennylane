dc8b32135d293d9163c47091faaaa10ed98719a8
Robert <robert.damoc@yahoo.com>
2021-06-08 17:54:37 +0300

    Filter recipes by ingredients

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,21 +1,43 @@
-class Api::V1::RecipesController < ApplicationController
-  def index
-    recipe = Recipe.order('RANDOM()').first(6)
+module Api
+  module V1
+    class RecipesController < ApplicationController
+      def index
+        params[:ingredients] = Ingredient.order('RANDOM()').limit(60).pluck(:title) if params[:test]
 
-    render json: recipe
-  end
+        recipe = if params[:ingredients]
+                   filter_by_ingredients
+                 else
+                   Recipe.order('RANDOM()').first(6)
+                 end
 
-  def show
-    if recipe
-      render json: recipe
-    else
-      render json: recipe.errors
+        render json: recipe
+      end
+
+      def show
+        if recipe
+          render json: recipe
+        else
+          render json: recipe.errors
+        end
+      end
+
+      private
+
+      def recipe
+        @recipe ||= Recipe.find(params[:id])
+      end
+
+      def filter_by_ingredients
+        ingredients = Ingredient.includes(:recipes).where(title: params[:ingredients])
+        ingredients_recipes = ingredients.flat_map(&:recipes).uniq
+
+        or_ingredients_identifier = ingredients.pluck(:identifier).map(&:to_i).reduce(&:|)
+
+        ingredients_recipes.select do |recipe|
+          identifier = recipe.ingredients_identifier.to_i
+          identifier & or_ingredients_identifier == identifier
+        end
+      end
     end
-  end
-
-  private
-
-  def recipe
-    @recipe ||= Recipe.find(params[:id])
   end
 end
